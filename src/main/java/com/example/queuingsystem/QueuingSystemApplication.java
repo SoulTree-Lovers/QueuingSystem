1efcdf590fc77459092f4a06af46aafba4fd68ec
kangseungmin <rkdtmdals999@naver.com>
2024-05-06 15:41:28 +0900

    feat: 메인 화면 컨트롤러 추가

--- src/main/java/com/example/queuingsystem/QueuingSystemApplication.java
+++ src/main/java/com/example/queuingsystem/QueuingSystemApplication.java
@@ -2,12 +2,20 @@ package com.example.queuingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class QueuingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QueuingSystemApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
 	}
 
 }
